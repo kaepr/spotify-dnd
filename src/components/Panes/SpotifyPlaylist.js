270e6c76dea988789fe61ddc3b6ac04b1bb41d84
Shagun Agrawal <agrawalshagun07@gmail.com>
2021-04-19 01:35:35 +0530

    delete unused

--- src/components/Panes/SpotifyPlaylist.js
+++
@@ -1,9 +0,0 @@
-import React, { useEffect, useState } from 'react';
-
-const SpotifyPlaylist = (props) => {
-  // console.log('props = ', props.data);
-
-  return <div className="flex-grow border-r-4 h-screen">Spotify Playlist</div>;
-};
-
-export default SpotifyPlaylist;
